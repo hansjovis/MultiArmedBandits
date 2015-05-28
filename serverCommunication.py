d5f5c6661fbc3c17b387661c8dd1023975ca2f9f
Hans-Christiaan Braun <hans-christiaan.braun@student.ru.nl>
2015-05-28 12:50:02 +0200

    First setup for communication with server.

---
+++ serverCommunication.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 12:35:49 2015
+
+@author: Hans-Christiaan
+"""
+
+TEAM_ID = "TheMulti-ArmedBandits"
+TEAM_PW = "???"
+
+def getContext(run_id, i):
+    """ Gets a simulated interaction from a user with the webpage
+        from the server.
+    """
+    return user_id, agent, language, age, referer,  
+
+def proposePage(run_id, i, header, ad_type, color, product_id, price):
+    """ Proposes the parameters of the ad to show to the interaction i
+        from run number run_id. Returns whether the user buys the product
+        or not.
+    """
+    return success
+
