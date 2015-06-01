a23f33aa9fb4aeb570ef4d0d72c42ae75c3610b0
Sennaa <senna_vaniersel@hotmail.com>
2015-06-01 21:00:10 +0200

    Handling of requests added

---
+++ handleRequests.py
@@ -0,0 +1,19 @@
+__author__ = 'Senna'
+
+import requests
+
+def get(url, params):
+    # make a get request
+    r = requests.get(url, params)
+    return r
+
+def post(url, params):
+    # make a post request
+    r = requests.post(url, params)
+    return r
+
+def getContent(r):
+    return r.text
+
+def getJSON(r):
+    return r.json()
