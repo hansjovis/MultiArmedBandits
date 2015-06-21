2daee3c2e5724409bf3a570ca0100ed35761a7dc
Harmen Prins <prins_harmen@hotmail.com>
2015-06-21 15:39:17 +0200

    Fully working version. Please check the configuration file.

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def main(max_its=None, verbose=True):
 	run_id = 0
 	if verbose: print "-- Starting --"
 	while not stopped:
-		run_id+=1
-		run_id = run_id % 50000
 		for i in range(50000):
 			context = serverCommunication.getContext(run_id, i)
 			ad_data = algorithm.make_selection(context)
@@ -18,12 +16,16 @@ def main(max_its=None, verbose=True):
 						ad_data['productid'], ad_data['price'])
 			result  = serverCommunication.proposePage(run_id, i, *datalist)
 			algorithm.learn(context, ad_data, result)
+			if verbose and i % config['updateinterval'] == 0:
+				print "Iteration #{}".format(i)
 			if i % config['saveinterval'] == 0:
 				algorithm.save()
 				if verbose: print "Saved"
-				if max_its is not None and run_id * 50000 + i > max_its:
-					if verbose: print "-- Stopping --"
-					return
+			if max_its is not None and run_id * 50000 + i > max_its:
+				if verbose: print "-- Stopping --"
+				return
+		run_id+=1
+		run_id = run_id % 50000
 
 if __name__ == "__main__":
 	main()
