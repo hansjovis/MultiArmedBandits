d915e8987494fd13050e3b35b3437e8467d4106c
Sennaa <senna_vaniersel@hotmail.com>
2015-06-29 16:35:08 +0200

    Implemented Greedy Epsilon (working?)

--- algorithm.py
+++ algorithm.py
@@ -1,58 +1,74 @@
 import pickle
 import os
 import dataHandler
+import random as rnd
 
 mapping = {'header': {0: '5', 1: '15', 2: '35'},
-		   'adtype': {0: 'skyscraper', 1: 'square', 2: 'banner'},
-		   'color':  {0: 'green', 1: 'blue', 2: 'red', 3: 'black', 4: 'white'},
-		  }
+           'adtype': {0: 'skyscraper', 1: 'square', 2: 'banner'},
+           'color': {0: 'green', 1: 'blue', 2: 'red', 3: 'black', 4: 'white'},
+           }
+
 
 class Algorithm:
-	def __init__(self, model=None):
-		""" Constructor for Algorithm.
+    def __init__(self, model=None, eta=None):
+        """ Constructor for Algorithm.
 			Optional arg model: if string, loads corresponding file
 								unless it is 'latest', then loads latest
 								if DataGenerator, uses that as model
 								else (if None) creates new model
 		"""
-		if model is None:
-			self.predictor = dataHandler.DataGenerator()
-		elif type(model) == str:
-			if model == 'latest':
-				i = 0
-				while os.path.exists("models/model{}".format(i)):
-					i+=1
-				self.predictor = pickle.load("models/model{}".format(i-1))
-			else:
-				self.predictor = pickle.load("models/" + model)
-		else:
-			self.predictor = model
-	
-	def convert(self, data):
-		""" Uses mapping as defined above to map integers to their
+        # Best lever is selected for a proportion 1 - eta
+        # Random lever is selected for a proportion eta
+        if eta is None:
+            self.eta = 0.1
+        else:
+            self.eta = eta
+
+        if model is None:
+            self.predictor = dataHandler.DataGenerator()
+        elif type(model) == str:
+            if model == 'latest':
+                i = 0
+                while os.path.exists("models/model{}".format(i)):
+                    i += 1
+                self.predictor = pickle.load("models/model{}".format(i - 1))
+            else:
+                self.predictor = pickle.load("models/" + model)
+        else:
+            self.predictor = model
+
+
+def convert(self, data):
+    """ Uses mapping as defined above to map integers to their
 			discrete representations.
 		"""
-		if 'productid' in data:
-			data['productid'] = data['productid'] + 10
-		if 'price' in data:
-			from decimal import Decimal
-			data['price'] = Decimal(data['price']).quantize(Decimal('0.01')).to_eng_string()
-		for key in data:
-			if key in mapping:
-				data[key] = mapping[key][data[key]]
-		return data
-	
-	def make_selection(self, context):
-		""" Expects a context of
+    if 'productid' in data:
+        data['productid'] = data['productid'] + 10
+    if 'price' in data:
+        from decimal import Decimal
+
+        data['price'] = Decimal(data['price']).quantize(Decimal('0.01')).to_eng_string()
+    for key in data:
+        if key in mapping:
+            data[key] = mapping[key][data[key]]
+    return data
+
+
+def make_selection(self, context):
+    """ Expects a context of
 			user_id, agent, language, age, referer
 			Produces a set of
 			header, ad_type, color, product_id, price
 			To learn from
 		"""
-		return self.convert(self.predictor.getPoint(context))
-	
-	def learn(self, context, ad_data, result):
-		""" Expects a context of
+    if(rnd.random < self.eta):
+        return self.convert(self.predictor.getPoint(context))
+    else:
+        return self.convert(self.predictor.getBestPoint(context))
+
+
+def learn(self, context, ad_data, result):
+    """ Expects a context of
 			user_id, agent, language, age, referer
 			ad_data:
 			header, ad_type, color, product_id, price
@@ -60,29 +76,33 @@ class Algorithm:
 			boolean or {success/failure}
 			To learn from
 		"""
-		pass
-	
-	def save(self, model=None):
-		""" Saves the model to new file """
-		if model is None:
-			model = self.lastmodel()
-		with open("models/model{}".format(model+1), 'wb') as openfile:
-			pickle.dump(self.predictor, openfile)
-	
-	def lastmodel(self):
-		i = 0
-		while os.path.exists("models/model{}".format(i)):
-			i+=1
-		return i-1
-	
-	def load(self, name=None):
-		if name is None:
-			name = self.lastmodel()
-		with open("models/" + name) as openfile:
-			return pickle.load(openfile)
+    # update distribution
+    self.predictor.updateDistribution()
 
-	def predict(self, context):
-		""" Like giveselection, but tries to maximize gain 
+def save(self, model=None):
+    """ Saves the model to new file """
+    if model is None:
+        model = self.lastmodel()
+    with open("models/model{}".format(model + 1), 'wb') as openfile:
+        pickle.dump(self.predictor, openfile)
+
+
+def lastmodel(self):
+    i = 0
+    while os.path.exists("models/model{}".format(i)):
+        i += 1
+    return i - 1
+
+
+def load(self, name=None):
+    if name is None:
+        name = self.lastmodel()
+    with open("models/" + name) as openfile:
+        return pickle.load(openfile)
+
+
+def predict(self, context):
+    """ Like giveselection, but tries to maximize gain
 			as opposed to information
 		"""
-		pass
+    pass
--- dataHandler.py
+++ dataHandler.py
@@ -11,88 +11,132 @@ import numpy as np
 
 
 types = {'header': 'ord',
-		 'adtype': 'disc',
-		 'color':  'disc',
-		 'productid': 'disc',
-		 'price': 'flt'}
+         'adtype': 'disc',
+         'color': 'disc',
+         'productid': 'disc',
+         'price': 'flt'}
+
 
 class Beta():
-	def __init__(self):
-		self.alpha = 1
-		self.beta  = 1
-	
-	def get(self):
-		return scipy.stats.beta(self.alpha, self.beta).rvs()
-	
-	def add(self, other):
-		self.alpha += other.alpha
-		self.beta  += other.beta
-	
+    def __init__(self):
+        self.alpha = 1
+        self.beta = 1
+
+    def get(self):
+        return scipy.stats.beta(self.alpha, self.beta).rvs()
+
+    def getBest(self):
+        return scipy.stats.beta(self.alpha, self.beta).mean()
+
+    def updateAlpha(self):
+        self.alpha += 1
+
+    def updateBeta(self):
+        self.beta += 1
+
+    def add(self, other):
+        self.alpha += other.alpha
+        self.beta += other.beta
+
+
 class Betas():
-	""" Class that covers multiple betas to use for discrete,
+    """ Class that covers multiple betas to use for discrete,
 		 non-ordinal variables"""
-	def __init__(self, n):
-		self.betas = [Beta() for i in range(n)]
-	
-	def get(self):
-		return [beta.get() for beta in self.betas]
-	
-	def add(self, other):
-		for beta,obeta in zip(self.betas, other.betas):
-			beta.add(obeta)
+
+    def __init__(self, n):
+        self.betas = [Beta() for i in range(n)]
+
+    def get(self):
+        return [beta.get() for beta in self.betas]
+
+    def getBest(self):
+        return [beta.getBest() for beta in self.betas]
+
+    def add(self, other):
+        for beta, obeta in zip(self.betas, other.betas):
+            beta.add(obeta)
+
 
 class Distribution():
-	""" Class that covers all distributions. Call get to get a random value.
+    """ Class that covers all distributions. Call get to get a random value.
 		Call fit to update the distribution.
 	"""
-	def __init__(self, dtype, shape, n=None, mx=None):
-		self.dtype = dtype
-		if dtype == 'ord':
-			self.distribution = Beta()
-			self.thresh = 1./n
-		elif dtype == 'disc':
-			self.n = n
-			self.distribution = Betas(n)
-		elif dtype == 'flt':
-			self.distribution = Beta()
-			self.mn = n
-			self.mx = mx
-	
-	def get(self):
-		if self.dtype == 'ord':
-			v = self.distribution.get()
-			return np.int(v/self.thresh)
-		elif self.dtype == 'disc':
-			vs = self.distribution.get()
-			return np.argmax(vs)
-		else:
-			v = self.distribution.get()
-			return self.mn + v * (self.mx - self.mn)
-	
-	def add(self, other):
-		self.distribution.add(other.distribution)
+
+    def __init__(self, dtype, shape, n=None, mx=None):
+        self.dtype = dtype
+        if dtype == 'ord':
+            self.distribution = Beta()
+            self.thresh = 1. / n
+        elif dtype == 'disc':
+            self.n = n
+            self.distribution = Betas(n)
+        elif dtype == 'flt':
+            self.distribution = Beta()
+            self.mn = n
+            self.mx = mx
+
+    def get(self):
+        if self.dtype == 'ord':
+            v = self.distribution.get()
+            return np.int(v / self.thresh)
+        elif self.dtype == 'disc':
+            vs = self.distribution.get()
+            return np.argmax(vs)
+        else:
+            v = self.distribution.get()
+            return self.mn + v * (self.mx - self.mn)
+
+    def getBest(self):
+        if self.dtype == 'ord':
+            v = self.distribution.getBest()
+            return np.int(v / self.thresh)
+        elif self.dtype == 'disc':
+            vs = self.distribution.getBest()
+            return np.argmax(vs)
+        else:
+            v = self.distribution.getBest()
+            return self.mn + v * (self.mx - self.mn)
+
+    def add(self, other):
+        self.distribution.add(other.distribution)
+
+    def updateDistribution(self, success):
+        if success:
+            self.distribution.updateAlpha()
+        else:
+            self.distribution.updateBeta()
 
 class DataGenerator():
-	def __init__(self, models=None):
-		if models is None:
-			self.distribution = {
-				'header': Distribution(types['header'], 'uniform', 3),
-				'adtype': Distribution(types['adtype'], 'uniform', 3),
-				'color':  Distribution(types['color'],  'uniform', 5),
-				'productid': Distribution(types['productid'], 'uniform', 16),
-				'price': Distribution(types['price'], 'uniform', 0, 50)
-			}
-		else:
-			self.merge(models)
-	
-	def getPoint(self, context):
-		point = {}
-		for key in self.distribution:
-			point.update({key: self.distribution[key].get()})
-		return point
+    def __init__(self, models=None):
+        if models is None:
+            self.distribution = {
+                'header': Distribution(types['header'], 'uniform', 3),
+                'adtype': Distribution(types['adtype'], 'uniform', 3),
+                'color': Distribution(types['color'], 'uniform', 5),
+                'productid': Distribution(types['productid'], 'uniform', 16),
+                'price': Distribution(types['price'], 'uniform', 0, 50)
+            }
+        else:
+            self.merge(models)
 
-	def merge(self, models):
-		self.distribution = models[0].distribution
-		for var in ['header', 'adtype', 'color', 'productid', 'price']:
-			for other in models[1:]:
-				self.distribution[var].add(other.distribution[var])
+    def getPoint(self, context):
+        point = {}
+        for key in self.distribution:
+            point.update({key: self.distribution[key].get()})
+        return point
+
+    def getBestPoint(self, context):
+        point = {}
+        for key in self.distribution:
+            point.update({key: self.distribution[key].getBest()})
+        return point
+
+    def updateDistribution(self, success):
+        for key in self.distribution:
+            self.distribution[key].updateDistribution(success)
+
+    def merge(self, models):
+        self.distribution = models[0].distribution
+        for var in ['header', 'adtype', 'color', 'productid', 'price']:
+            for other in models[1:]:
+                self.distribution[var].add(other.distribution[var])
